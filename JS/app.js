111e884068e772337069f3416265903782b2d474
Steven <stevccru@icloud.com>
2022-06-08 21:12:33 -0400

    comments added for now, able to get first section header to highlight

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 let sections  = document.querySelectorAll('section');
 //variable targeting sections of window
 
+//postions will be an empty array
 let positions = [];
 
 //variable positions will be set to an empty array
@@ -13,15 +14,17 @@ for (let i = 0; i < sections.length; i++) {
 function setupNav() {
     //function for setting up the nav bar
   let navList = document.getElementById('navbar__list');
+  //defining the navlist as a variable and reaching out to the DOM to retrieve the ID.
 
   for (let i = 0; i < sections.length; i++) {
+      //Setting up a for loop to cycle through every section.
     let listItem = document.createElement('li');//add class here
-    let section = sections[i];
-    let sectionId = section.getAttribute('id');
-    let sectionTitle = section.getAttribute('data-nav');
+    let section = sections[i];//This variable defines a section picked out of the array of Sections.
+    let sectionId = section.getAttribute('id');//fetching the attribute id and defining it as variable sectionId.
+    let sectionTitle = section.getAttribute('data-nav');//fetching the data nav attribute and defining it as sectionId.
     
-    listItem.setAttribute('data-section', sectionId );
-    listItem.innerHTML = sectionTitle;
+    listItem.setAttribute('data-section', sectionId );//setting the attribute data-section (which does not cuurently exist) and passing the value of variable sectionId
+    listItem.innerHTML = sectionTitle;//Taking the listItem variable we created earlier and updating the inner HTML to the sectionTitle variable/
     navList.appendChild(listItem);
 
     listItem.addEventListener('click', function(event){
@@ -31,6 +34,7 @@ function setupNav() {
         window.scroll({
             top: nextSection.offsetTop,
             behavior: "smooth"
+
         });
 
     
@@ -40,6 +44,7 @@ function setupNav() {
   }
 }
 function removeActiveStateFromSections(){
+   
     for(let i = 0; i < sections.length; i++){
         sections[i].classList.remove('active-section');
     }
@@ -48,6 +53,7 @@ function removeActiveStateFromSections(){
 
 window.addEventListener("scroll", function(){
     let currentPosition = window.pageYOffset;
+    let sectionHeader = document.querySelector('h2');
     
     for(let i = 0; i < positions.length; i++){
         if (currentPosition >= positions[i] && currentPosition <= positions[i + 1] ){
@@ -56,13 +62,15 @@ window.addEventListener("scroll", function(){
 
             sections[i].classList.add('active-section');
 
+            sectionHeader.style.color = "yellow";
+
 
         }else if(currentPosition >= positions[i]){
             removeActiveStateFromSections();
 
             sections[i].classList.add('active-section');
 
-
+            
             
         }
 
